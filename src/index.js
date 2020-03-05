be1778387800c8c0e7c8bb97dd79ddb637121d43
Alex Pleshkou <pla@tut.by>
2020-03-05 20:53:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =  function check(str, bracketsConfig) {
+  if (str.length % 2) {return false};
+  let brackets = [];
+  for (n of bracketsConfig) {
+    brackets.push(n[0] + n[1]);
+  }
+
+  while (true) {
+    let start = str.length;
+    brackets.forEach(pair => {str = str.split(pair).join('')})
+    if (start == str.length) break;
+  }
+  return str.length ? false : true;
 }
